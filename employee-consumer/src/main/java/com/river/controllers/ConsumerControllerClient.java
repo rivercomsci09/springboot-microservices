b76fc3a8b9ebf668cc38f921354c491d70b4ff3a
Tran Truong Giang <rivercomsci09@gmail.com>
2020-05-19 15:28:57 +0700

    Zuul

--- employee-consumer/src/main/java/com/river/controllers/ConsumerControllerClient.java
+++ employee-consumer/src/main/java/com/river/controllers/ConsumerControllerClient.java
@@ -4,9 +4,11 @@ import com.river.model.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -25,5 +27,17 @@ public class ConsumerControllerClient {
         List<Object> employees = restTemplate.getForObject("http://employee-producer/employees/", List.class);
         department.setEmployees(employees);
         return department;
+    }
+
+    @RequestMapping(value = "/departments", method = RequestMethod.GET)
+    public List<Department> getDepartments(){
+        // get list of available employees
+        List<Object> employees = restTemplate.getForObject("http://employee-producer/employees/", List.class);
+        List<Department> departments = Arrays.asList(
+                new Department(1,employees),
+                new Department(2,employees),
+                new Department(3,employees)
+        );
+        return departments;
     }
 }
